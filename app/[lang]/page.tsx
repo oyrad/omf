ae8bc9f15feb5b5fb380aafc80d455889aa2b886
oyrad <dario.susanj2@gmail.com>
2023-12-28 10:45:51 +0100

    add about us section

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,6 +3,8 @@ import { getDictionary } from "@/lib/dictionary";
 import Image from "next/image";
 import logo from "/public/logo.svg";
 import backgroundImage from "/public/bg.png";
+import skyscraper from "/public/skyscraper.png";
+import Link from "next/link";
 
 export default async function Home({
   params: { lang },
@@ -33,12 +35,38 @@ export default async function Home({
         <h4 className="text-4xl font-bold text-stone-500 mb-2">
           {page.home.about}
         </h4>
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 mb-8">
           <div className="h-1.5 w-8 bg-stone-800" />
           <p className="text-stone-800 text-3xl font-bold">
             {page.home.mission}
           </p>
         </div>
+        <div className="flex justify-between items-center gap-44 mb-48">
+          <div className="flex flex-col items-start space-y-8 basis-1/2">
+            <p>{page.home.firstAboutParagraph}</p>
+            <div className="flex space-x-4">
+              <div className="h-18 w-12 bg-stone-300" />
+              <p className="font-bold italic">
+                {page.home.secondAboutParagraph}
+              </p>
+            </div>
+            <p>{page.home.thirdAboutParagraph}</p>
+            <p>{page.home.fourthAboutParagraph}</p>
+            <button className="bg-stone-800 text-white px-4 py-1">
+              <Link href={`/${lang}/contact`}>
+                {page.home.contactButtonText}
+              </Link>
+            </button>
+          </div>
+          <Image src={skyscraper} alt="skyscraper" className="w-1/3" />
+        </div>
+        <h4 className="text-4xl font-bold text-stone-500 mb-2">
+          {page.home.services}
+        </h4>
+        <p className="text-stone-800 text-4xl font-bold mb-4">
+          {page.home.forClients}
+        </p>
+        <div className="h-1.5 w-20 bg-stone-800 mb-48" />
       </div>
     </>
   );
--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -6,10 +6,4 @@ const dictionaries = {
   hr: () => import("@/dictionaries/hr.json").then((module) => module.default),
 };
 
-export const getDictionary = async (locale: Locale) => {
-  if (!(locale === "en" || locale === "hr")) {
-    throw new Error('Invalid locale. Supported locales are "en" and "hr".');
-  }
-
-  return dictionaries[locale]();
-};
+export const getDictionary = async (locale: Locale) => dictionaries[locale]();
