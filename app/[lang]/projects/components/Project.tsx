3b352a1e92c65a7e1e0102008819a8207654efc3
oyrad <dario.susanj2@gmail.com>
2024-01-03 19:05:15 +0100

    add arrow icon to project

--- app/[lang]/projects/components/Project.tsx
+++ app/[lang]/projects/components/Project.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import {
+  ArrowRight,
+  // @ts-ignore
+} from "@phosphor-icons/react/dist/ssr";
+
 type ProjectProps = {
   image: string;
   title: string;
@@ -17,8 +22,13 @@ export default function Project({ image, title, description }: ProjectProps) {
         width={600}
         className="mb-2"
       />
-      <p className="font-semibold">{title}</p>
-      <p className="text-sm text-stone-400">{description}</p>
+      <div className="flex justify-between items-center">
+        <div>
+          <p className="font-semibold">{title}</p>
+          <p className="text-sm text-stone-400">{description}</p>
+        </div>
+        <ArrowRight className="w-8 h-8 p-1 bg-stone-200" />
+      </div>
     </Link>
   );
 }
