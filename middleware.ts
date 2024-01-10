b749a4221da9aa315bfee00a6d457b2a95a4c956
oyrad <dario.susanj2@gmail.com>
2024-01-10 21:07:15 +0100

    add button hover effect

--- middleware.ts
+++ middleware.ts
@@ -9,8 +9,21 @@ export function middleware(request: NextRequest) {
   }
 
   if (
+    pathname === "/admin" ||
+    pathname === "/hr/admin" ||
+    pathname === "/en/admin"
+  ) {
+    return NextResponse.redirect(
+      new URL(
+        "https://app.contentful.com/spaces/woj3j3wz3tf0/views/entries?searchText=&contentTypeId=&contentTypeIds=project&displayedFieldIds=contentType&displayedFieldIds=updatedAt&displayedFieldIds=author&order.direction=descending&order.fieldId=updatedAt&filters="
+      )
+    );
+  }
+
+  if (
     pathname !== "/en" &&
     pathname !== "/hr" &&
+    pathname !== "/admin" &&
     pathname.split("/").length === 2
   ) {
     return NextResponse.redirect(new URL("/hr", request.url));
